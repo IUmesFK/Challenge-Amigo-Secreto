aa41664936c5aa6ac5ba7478c2863dfdc308878f
eze <ezep25728@gmail.com>
2025-01-22 20:33:11 -0300

    Se definen las funciones a implementar

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigos() {
+    
+}
+
+function actualizarListaAmigos() {
+    
+}
+
+function sortearAmigos() {
+    
+}
