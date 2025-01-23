db31bdcb8bc2c8dd97b574d8f5623e3d8d057f2e
eze <ezep25728@gmail.com>
2025-01-23 08:36:49 -0300

    implementación de la función actualizarListaAmigos()

--- app.js
+++ app.js
@@ -10,10 +10,19 @@ function agregarAmigos() {
     }
     document.getElementById("amigo").value = "";
     console.log(amigos);
+    return;
 }
 
-function actualizarListaAmigos() {
-    
+function actualizarListaAmigos(listaAmigos) {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = '';
+    for(let i = 0; i <= listaAmigos.length-1; i++){
+        let elementoLista = document.createElement("li");
+        let nodoDeTexto = document.createTextNode(listaAmigos[i]);
+        elementoLista.appendChild(nodoDeTexto);
+        lista.appendChild(elementoLista);
+    }
+    return;
 }
 
 function sortearAmigos() {
